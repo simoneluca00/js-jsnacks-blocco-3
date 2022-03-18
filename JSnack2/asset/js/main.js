381aa74e97fa708735a07f0353887d278b6a9c3a
simoneluca00 <si.zaino@libero.it>
2022-03-18 11:09:39 +0100

    JSnack4 correzione smart (definitiva)

--- JSnack2/asset/js/main.js
+++ JSnack2/asset/js/main.js
@@ -1,67 +1,66 @@
 // Snack2
 
-//ANCHOR 1) Fai inserire un numero, che chiameremo "n", all’utente.
-let n = parseInt(prompt("Quante liste vuoi creare? (inserire valore numerico)")); 
-if (isNaN(n)) {
-    n = parseInt(prompt("Inserire valore numerico")); 
+const x = parseInt(prompt("inserisci numero"));
+
+let y = 0;
+
+while (y < x) {
+    let newArray = [];
+    
+    for ( let z = 0; z < 10; z++ ) {
+        newArray.push( Math.floor(Math.random() * 100) +1 );
+    }
+    
+        document.getElementById("containerArray").innerHTML += ` <div style="font-weigth: bold; padding: 30px;"> <span style="text-decoration: underline;">Lista n.${y+1}:</span> ${newArray} </div> `;
+    console.log( newArray );
+    y++
 }
 
+// ------------------------------------ CORREZIONE ALTERNATIVA MIA ------------------------------------ //
+//ANCHOR 1) Fai inserire un numero, che chiameremo "n", all’utente.
+// let n = parseInt(prompt("Quante liste vuoi creare? (inserire valore numerico)")); 
+// if (isNaN(n)) {
+//     n = parseInt(prompt("Inserire valore numerico")); 
+// }
+
 //ANCHOR 2) Genera "n" array, ognuno formato da 10 numeri casuali da 1 a 100. 
+// let i = 0;
 
-let i = 0;
-
-do {
+// do {
     
-    let numeriRandom = Math.floor(Math.random() * 100 + 1);
-    let numeriRandom1 = Math.floor(Math.random() * 100 + 1);
-    let numeriRandom2 = Math.floor(Math.random() * 100 + 1);
-    let numeriRandom3 = Math.floor(Math.random() * 100 + 1);
-    let numeriRandom4 = Math.floor(Math.random() * 100 + 1);
-    let numeriRandom5 = Math.floor(Math.random() * 100 + 1);
-    let numeriRandom6 = Math.floor(Math.random() * 100 + 1);
-    let numeriRandom7 = Math.floor(Math.random() * 100 + 1);
-    let numeriRandom8 = Math.floor(Math.random() * 100 + 1);
-    let numeriRandom9 = Math.floor(Math.random() * 100 + 1);
+//     let numeriRandom = Math.floor(Math.random() * 100 + 1);
+//     let numeriRandom1 = Math.floor(Math.random() * 100 + 1);
+//     let numeriRandom2 = Math.floor(Math.random() * 100 + 1);
+//     let numeriRandom3 = Math.floor(Math.random() * 100 + 1);
+//     let numeriRandom4 = Math.floor(Math.random() * 100 + 1);
+//     let numeriRandom5 = Math.floor(Math.random() * 100 + 1);
+//     let numeriRandom6 = Math.floor(Math.random() * 100 + 1);
+//     let numeriRandom7 = Math.floor(Math.random() * 100 + 1);
+//     let numeriRandom8 = Math.floor(Math.random() * 100 + 1);
+//     let numeriRandom9 = Math.floor(Math.random() * 100 + 1);
 
-    let lista = new Array(10);
+//     let lista = new Array(10);
     
-    lista[0] = numeriRandom;
-    lista[1] = numeriRandom1;
-    lista[2] = numeriRandom2;
-    lista[3] = numeriRandom3;
-    lista[4] = numeriRandom4;
-    lista[5] = numeriRandom5;
-    lista[6] = numeriRandom6;
-    lista[7] = numeriRandom7;
-    lista[8] = numeriRandom8;
-    lista[9] = numeriRandom9;
+//     lista[0] = numeriRandom;
+//     lista[1] = numeriRandom1;
+//     lista[2] = numeriRandom2;
+//     lista[3] = numeriRandom3;
+//     lista[4] = numeriRandom4;
+//     lista[5] = numeriRandom5;
+//     lista[6] = numeriRandom6;
+//     lista[7] = numeriRandom7;
+//     lista[8] = numeriRandom8;
+//     lista[9] = numeriRandom9;
     
-    console.log(lista);
+//     console.log(lista);
 
-    lista = lista.join(', ');
+//     lista = lista.join(', ');
 
     //ANCHOR 3) Ogni volta che ne crei uno, stampalo.
-    document.getElementById("containerArray").innerHTML += ` <div style="font-weigth: bold; padding: 30px;"> <span style="text-decoration: underline;">Lista n.${i+1}:</span> ${lista} </div> `;
 
-    i++
-} while (i < n);
+//     i++
+// } while (i < n);
 
-// ------------------------------------ CORREZIONE ALTERNATIVA ------------------------------------ //
-
-// const x = parseInt(prompt("inserisci numero"));
-
-// let y = 0;
-
-// while (y < x) {
-//     let newArray = [];
-
-//     for ( let z = 0; z < 10; z++ ) {
-//         newArray.push( Math.floor(Math.random() * 100) +1 );
-//     }
-
-//     console.log( newArray );
-//     y++
-// }
 
 
 
