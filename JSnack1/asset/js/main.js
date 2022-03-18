9d945a0ca500aaaf74242f8206950a2b39e95255
simoneluca00 <si.zaino@libero.it>
2022-03-18 10:06:08 +0100

    JSnack1 correzione

--- JSnack1/asset/js/main.js
+++ JSnack1/asset/js/main.js
@@ -1,12 +1,27 @@
 // Snack1
-// Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.
+// Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. 
+// Continua a chiedere i numeri all’utente e a inserirli nell’array 
+// fino a quando la somma degli elementi è minore di 50.
  
 let arrayList = [];
 let sum = 0;
 
 do {
     let richiesta = parseInt(prompt("Inserisci un numero"));
+    
+    arrayList.push(richiesta);
+    
     sum += richiesta;
+    
+    if (sum > 50) {
+        var arrayListPop = arrayList.pop();
+        arrayListPop;
+    }
+
+    let arrayListString = arrayList.join(", ");
+    
+    document.getElementById("containerList").innerHTML = `La lista dei numeri inseriti è: <div style="font-size: 2em; font-weight:bold; margin-top: 20px;">${arrayListString}</div>`;
+    document.getElementById("containerSum").innerHTML = `La somma dei numeri inseriti è: <div style="font-size: 2em; font-weight:bold; margin-top: 20px;">${sum - arrayListPop}</div>`;
+
 } while (sum < 50);
 
-document.getElementById("containerSum").innerHTML += `La somma dei numeri inseriti è: <div style="font-size: 2em; font-weight:bold; margin-top: 20px;">${sum}</div>`
